d32a6ccb462d53d65f108e25a2d9a5e01238a706
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-06-19 10:57:59 +0700

    Organize directory

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "antd/lib/button";
+import Icon from "antd/lib/icon";
+import "./index.css";
+
+function Home() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+      <Icon type="step-backward" />
+    </div>
+  );
+}
+
+export default Home;
